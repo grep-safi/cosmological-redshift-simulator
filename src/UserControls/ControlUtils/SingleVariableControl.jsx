8ba091da0360390c174498d1600e1b5dc2d886b1
grep-safi <hub.of.safi@gmail.com>
2020-03-19 19:41:58 -0700

    set up control utils

---
+++ src/UserControls/ControlUtils/SingleVariableControl.jsx
@@ -0,0 +1,84 @@
+import React from "react";
+import NumberInputField from "./NumberInputField";
+
+/**
+ * A Single Variable Control is a combination of both a number input box and a
+ * slider.  It can be used to adjust a single "planetary parameter" at a time.
+ * @extends React
+ */
+
+class SingleVariableControl extends React.Component {
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+        const value = Number.parseFloat(this.props.value).toFixed(this.props.decimals);
+        return (
+            <label>
+                {this.props.displayName}:&nbsp;
+                <NumberInputField
+                    type="number"
+                    name={this.props.name}
+                    min={this.props.min}
+                    max={this.props.max}
+                    step={this.props.step}
+                    onNewValue={this.handleNewValue.bind(this)}
+                    value={this.props.value}
+                    decimals={this.props.decimals}
+                />
+                <input
+                    type="range"
+                    name={this.props.name}
+                    min={this.props.min}
+                    max={this.props.max}
+                    step={this.props.step}
+                    onChange={this.handleChange.bind(this)}
+                    value={value}
+                />
+            </label>
+        )
+    }
+
+    handleNewValue(value) {
+        let name = this.props.name;
+        value = this.convertEntryToValidNumber(value);
+        this.props.onChange(name, value);
+    }
+
+    handleChange(event) {
+        let name = this.props.name;
+        let value = this.convertEntryToValidNumber(event.target.value);
+        this.props.onChange(name, value);
+    }
+
+    /**
+     * Converts string into a number, and ensures that it is within the valid
+     * range of numbers, using the "min" and "max" provided by the props passed
+     * to this component.
+     * @param  {String} value The direct input string from user.
+     * @return {Number} The validated number output
+     */
+    convertEntryToValidNumber(value) {
+        let type = typeof(value);
+        if (isNaN(value) || type !== 'string' && type !== 'number') {
+            return this.props.min;
+        }
+        value = Number.parseFloat(value);
+        value = Math.min(this.props.max, value);
+        value = Math.max(this.props.min, value);
+        return value;
+    }
+
+}
+
+SingleVariableControl.propTypes = {
+    name: PropTypes.string,
+    min: PropTypes.number,
+    max: PropTypes.number,
+    step: PropTypes.number,
+    value: PropTypes.number,
+    decimals: PropTypes.number,
+    onChange: PropTypes.func,
+    displayName: PropTypes.string,
+}
---
+++ src/UserControls/Parameters.jsx
@@ -0,0 +1,70 @@
+import React from 'react';
+import PropTypes from 'prop-types';
+import SingleVariableControl from "./ControlUtils/SingleVariableControl";
+
+/**
+ * Parameters is a GUI interface that controls the
+ * variables used to alter the orbit drawings in the OrbitView.
+ */
+export default class Parameters extends React.Component {
+    constructor(props) {
+        super(props);
+        this.handleSingleVariableChange = this.handleSingleVariableChange.bind(this);
+        this.handleChange = this.handleChange.bind(this);
+    }
+
+    render() {
+        return (
+            <React.Fragment>
+                <h2>Redshift Controls Parameters</h2>
+                <br/>
+                <fieldset>
+                    <legend>Parameters</legend>
+                    <SingleVariableControl
+                        name="InitialSeparationDistance"
+                        displayName="Initial Separation Distance"
+                        min={100}
+                        max={250}
+                        step={0.01}
+                        decimals={2}
+                        value={this.props.params.initSeparationDist}
+                        onChange={this.handleSingleVariableChange}
+                    />
+                    <br/>
+                    <SingleVariableControl
+                        name="expansionRate"
+                        displayName="Expansion Rate"
+                        min={3.00}
+                        max={12.00}
+                        step={0.01}
+                        decimals={2}
+                        value={this.props.params.expansionRate}
+                        onChange={this.handleSingleVariableChange}
+                    />
+                    <br/>
+                </fieldset>
+                <br/>
+            </React.Fragment>
+        )
+    }
+
+    handleChange(newParams) {
+        this.props.onChange(newParams);
+    }
+
+    handleSingleVariableChange(key, value) {
+        this.props.onChange({
+            ...this.props.params,
+            [key]: value
+        });
+    }
+}
+
+Parameters.propTypes = {
+    onChange: PropTypes.func.isRequired,
+    params: PropTypes.exact({
+        initSeparationDist: PropTypes.number.isRequired,
+        expansionRate: PropTypes.number.isRequired,
+    }).isRequired,
+}
+
